acce641369950926e89e49b09e9dac1eb2b26c5c
Martin Ring <ring.martin@gmail.com>
2021-07-17 15:04:22 -0300

    improved cart card

--- components/Cart.js
+++ components/Cart.js
@@ -5,21 +5,24 @@ import Button from './Button';
 export default function Cart() {
   return (
     <div className={s.wrapper}>
+      <div className={s.bg}></div>
       <div className={s.container}>
-        <div className={s.titleContainer}>
-          <h3 className={s.title}>CART (3)</h3>
-          <p className={s.removeAll}>Remove All</p>
+        <div className={s.cart}>
+          <div className={s.titleContainer}>
+            <h3 className={s.title}>CART (3)</h3>
+            <p className={s.removeAll}>Remove All</p>
+          </div>
+          <div className={s.cartItems}>
+            <CartItem />
+            <CartItem />
+            <CartItem />
+          </div>
+          <div className={s.totalContainer}>
+            <p className={s.total}>TOTAL</p>
+            <p className={s.amount}>$5,567</p>
+          </div>
+          <Button type={"one"} text={"checkout"} style={s.fullWidth} />
         </div>
-        <div className={s.cartItems}>
-          <CartItem />
-          <CartItem />
-          <CartItem />
-        </div>
-        <div className={s.totalContainer}>
-          <p className={s.total}>TOTAL</p>
-          <p className={s.amount}>$5,567</p>
-        </div>
-        <Button type={"one"} text={"checkout"} style={s.fullWidth} />
       </div>
     </div>
   )
